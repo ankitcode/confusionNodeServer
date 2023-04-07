bec3c83b63cda0d81f4dfdc15ba6413a09b4cb9b
Ankit <ankit93100@gmail.com>
2023-04-07 23:28:34 +0530

    oAuth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: "12345-67890-09876-54321",
   mongoUrl: "mongodb://localhost:27017/conFusion",
+  facebook: {
+    clientId: "749082280215190",
+    clientSecret: "d744a159f785c17b25254a65dc7eec26",
+  },
 };
